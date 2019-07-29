b7d0c5de3cfa59f36efc26005add23625750e1c7
Andreas Dewes <andreas.dewes@kiprotect.com>
2019-07-29 22:36:53 +0200

    Worked on PDP implementation.

--- algoneer/methods/blackbox/pdp.py
+++ algoneer/methods/blackbox/pdp.py
@@ -9,7 +9,6 @@ learning models.
 from typing import Sequence, Optional
 from algoneer import DataSet, Model, ModelTest
 
-
 class PDP(ModelTest):
 
     """
@@ -20,13 +19,54 @@ class PDP(ModelTest):
         super().__init__()
 
     def run(
-        self, model: Model, dataset: Optional[DataSet] = None, features: Optional[Sequence[str]] = None
+        self, model: Model, dataset: DataSet, features: Optional[Sequence[str]] = None
     ):
         """
         Run the test.
 
         :param    model: The model for which to generate the PDP.
-        :param  dataset: The dataset for which to generate the PDP. Optional.
+        :param  dataset: The dataset for which to generate the PDP.
         :param features: The features for which to generate the PDP. Optional.
         """
-        pass
+
+        def pdp(ds, model, feature):
+            """
+            Generate the partial dependence for a categorical feature
+            """
+            values = dataset[feature]
+            if feature.is_categorical or feature.is_ordinal:
+                # we get all unique values for the feature
+                vs = values.unique()
+            elif feature.is_numerical:
+                vs = values.unique()
+            else:
+                raise ValueError("unknown feature type: {}".format(feature.type))
+
+            vs.assign(pdp=0)
+
+            for i, v in vs.iterrows():
+                # we make a copy of the dataset (this might be expensive)
+                nds = ds.copy()
+                # we replace all values of the feature by v
+                nds[feature] = v
+                # we compute the prediction of the model for the modified data
+                y = model.predict(nds)
+                if model.is_classifier:
+                    # to do: handle multi-class classifiers
+                    # if this is a classifier we calculate the probability
+                    vs[i, 'pdp'] = y.sum()/len(y)
+                elif model.is_regression:
+                    # if this is a regression, we calculate the mean value
+                    vs[i, 'pdp'] = y.mean()
+
+            return vs
+
+        # we store PDPs in a simple dict
+        pdps = {}
+
+        # we generate a partial dependence plot for every feature
+        for feature in dataset.features:
+            pdps[feature] = pdp(dataset, model, feature)
+
+        # we return the PDPs
+        return pdps
