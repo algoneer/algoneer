3b6ce9f973a782925e1285f9f4efd9f2fc627d82
Andreas Dewes <andreas.dewes@kiprotect.com>
2019-08-02 14:36:47 +0200

    v0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from req import reqs
 setup(
     name="algoneer",
     python_requires=">=3.6",
-    version="0.0.4",
+    version="0.0.5",
     author="Andreas Dewes",
     author_email="authors@algoneer.org",
     license="Mozilla Public License (MPL)",
