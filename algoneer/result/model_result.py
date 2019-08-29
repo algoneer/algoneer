21393bb2f83883739c9003cce8eb1d7b1ed649cb
Andreas Dewes <andreas.dewes@kiprotect.com>
2019-08-29 10:32:44 +0200

    Added optional datapoint model results to model result class.

--- algoneer/result/model_result.py
+++ algoneer/result/model_result.py
@@ -1,10 +1,23 @@
 import algoneer
 from .result import Result
+from .datapoint_model_result import DatapointModelResult
 
-from typing import Any
+from typing import Any, Optional, Iterable
 
 
 class ModelResult(Result):
-    def __init__(self, data: Any, model: "algoneer.Model"):
+
+    """
+    Contains a result for a given model, and optionally results for individual
+    datapoints for the model.
+    """
+
+    def __init__(
+        self,
+        data: Any,
+        model: "algoneer.Model",
+        datapoint_results: Optional[Iterable[DatapointModelResult]] = None,
+    ):
         super().__init__(data)
         self.model = model
+        self.datapoint_results = datapoint_results
