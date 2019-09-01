3f03cd57060ca1094c684b0e6e9d84b2d9d2fbf8
Andreas Dewes <andreas.dewes@kiprotect.com>
2019-09-01 21:36:35 +0200

    Update API test.

--- algoneer_tests/api/test_api.py
+++ algoneer_tests/api/test_api.py
@@ -1,6 +1,7 @@
 from algoneer_datasets.bike_sharing import load_dataset
 from algoneer_datasets.bike_sharing.algorithms import get_algorithm
 from algoneer.dataschema import AttributeSchema, DataSchema
+from algoneer.methods.blackbox.ale import ALE, ALEResult
 from algoneer.api import Session, Response, Object as APIObject
 from algoneer.api.base_client import BaseClient
 from algoneer.object import Object
@@ -43,15 +44,26 @@ class ApiTest(unittest.TestCase):
         assert isinstance(model, Object)
         assert isinstance(dataset, Object)
 
+
+        # we initialize a ALE test
+        ale = ALE()
+
+        columns = ['windspeed', 'hum', 'atemp', 'season']
+
+        # we run the ALE test on the model and dataset
+        result = ale.run(dataset, model, columns=columns, max_datapoints=200, n_intervals=10)
+
         client = Client()
         session = Session(client)
 
         api_model = session.add(model)
         api_dataset = session.add(dataset)
         api_algo = session.add(algo)
+        api_result = session.add(result)
 
         assert model in session
         assert dataset in session
         assert algo in session
+        assert result in session
 
         session.sync()
